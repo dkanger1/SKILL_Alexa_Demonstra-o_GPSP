d7ac1a9322ecb1d07a8448353a2d44e4717cc34a
AlexaHostedSkills <VoiceHubCodeCommitRole/VoiceHubCodeCommitRole>
2021-01-13 22:03:07 +0000

    updated lambda/index.js.

--- lambda/index.js
+++ lambda/index.js
@@ -34,7 +34,7 @@ const TarefaMaisLongaIntentHandler = {
   
       return handlerInput.responseBuilder
         .speak(outputSpeech)
-     //   .reprompt('Você pode querer saber qual foi a tarefa mais curta, o resumo do dia ou o resumo dos colaboradores')
+        .reprompt('Você pode querer saber qual foi a tarefa mais curta, o resumo do dia ou o resumo dos colaboradores')
         .getResponse();
     },
   };
@@ -61,7 +61,7 @@ const TarefaMaisCurtaIntentHandler = {
   
       return handlerInput.responseBuilder
         .speak(outputSpeech)
-   //     .reprompt('você pode querer saber qual foi a tarefa mais curta, o resumo do dia ou o resumo dos colaboradores')
+        .reprompt()
         .getResponse();
     },
   };
@@ -87,7 +87,7 @@ const ResumoExecutantesIntentHandler = {
   
       return handlerInput.responseBuilder
         .speak(outputSpeech)
-   //     .reprompt('você pode querer saber qual foi a tarefa mais curta, o resumo do dia ou o resumo dos colaboradores')
+        .reprompt()
         .getResponse();
     },
   };
@@ -113,7 +113,7 @@ const ResumoDoDiaIntentHandler = {
   
       return handlerInput.responseBuilder
         .speak(outputSpeech)
-       // .reprompt('você pode querer saber qual foi a tarefa mais curta, o resumo do dia ou o resumo dos colaboradores')
+        .reprompt()
         .getResponse();
     },
   };
@@ -139,7 +139,7 @@ const QuemExecutouIntentHandler = {
   
       return handlerInput.responseBuilder
         .speak(outputSpeech)
-        .reprompt('você pode querer saber qual foi a tarefa mais curta, o resumo do dia ou o resumo dos colaboradores')
+        .reprompt()
         .getResponse();
     },
   };
@@ -149,7 +149,7 @@ const HelpIntentHandler = {
             && Alexa.getIntentName(handlerInput.requestEnvelope) === 'AMAZON.HelpIntent';
     },
     handle(handlerInput) {
-        const speakOutput = 'Você pode consultar um resumo do dia,  analisar um resumo dos colaboradores e saber qual foi a tarefa mais longa ou a mais curta.';
+        const speakOutput = 'Você pode consultar um resumo do dia, saber qual foi a tarefa mais longa ou a mais curta, analisar um resumo dos colaboradores';
 
         return handlerInput.responseBuilder
             .speak(speakOutput)
